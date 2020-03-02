b01db5b206651233b16487d365f507f0abbe8fb2
AnusManus <bfskinner60659@gmail.com>
2020-03-03 01:30:22 +0300

    setting up

--- Assembly/includes/tokens.h
+++ Assembly/includes/tokens.h
@@ -2,6 +2,22 @@
 # define TOKENS_H
 
 typedef struct s_line_token t_line_token;
+typedef struct s_token t_token;
+
+# define FILE_ERROR_MESSAGE "Could not open the file;\n"
+# define SPACES " \t"
+
+enum e_token_type
+{
+    unknown,
+    command,
+    string,
+    label,
+    operation,
+    argument,
+    comment,
+    new_line
+};
 
 struct s_line_token
 {
@@ -9,6 +25,12 @@ struct s_line_token
     char *label;
     char *operation;
     char *arguments;
+};
+
+struct s_token
+{
+    char *string;
+    enum e_token_type type;
 };
 
 #endif
--- Assembly/test_run.c
+++ Assembly/test_run.c
@@ -4,24 +4,66 @@
 #include "generic_list.h"
 #include "tokens.h"
 #include "function_prototypes.h"
+#include <fcntl.h>
 
-
-
-char *get_label(char *line, t_line_token *token)
+t_token *new_token(char *string, enum e_token_type type)
 {
-    ;
+    t_token *token;
+
+    token = mallokill(sizeof(t_token));
+    token->string = string;
+    if (type == 0)
+        token->type = unknown;
+    else
+        token->type = type;
+    return (token);
 }
 
-t_line_token *new_line_token(char *line)
+void skip_spaces(int *n, char *line)
 {
-    t_line_token *token;
+    while (ft_strstr(SPACES, &line[*n]) != NULL)
+        *n = *n + 1;
+}
 
-    token = mallokill(sizeof(t_line_token));
-    token->index = 0;
+t_generic_list *line_to_tokens(char *line)
+{
+    int n;
+    t_token *token;
+    t_generic_list *token_list;
 
+    n = 0;
+    token = NULL;
+    token_list = NULL;
+    // skip_spaces(&n, line);
+    while (line[n] != '\0')
+    {
+        if (ft_strstr(SPACES, &line[n]) != NULL)
+            skip_spaces(&n, line);
+        
+        n = n + 1;
+    }
+    return (token_list);
 }
 
 int main()
 {
+    char *current_line;
+    int file;
+    t_generic_list *tokens;
+
+    file = open("Makefile", O_RDONLY);
+    if (file < 0)
+    {
+        // ft_printf("%s", FILE_ERROR_MESSAGE);
+        write(1, FILE_ERROR_MESSAGE, ft_strlen(FILE_ERROR_MESSAGE));
+        exit(1);
+    }
+    while (get_next_line(file, &current_line) > 0)
+    {
+        ft_printf("%s\n", current_line);
+        // line_to_tokens(current_line);
+        ;
+    }
+    
     return (0);
 }
