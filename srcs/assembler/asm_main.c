1c191292354f2dc6e20391878d93c1fdc95c0397
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-26 20:16:56 +0300

    asm_main.c norm

--- srcs/assembler/asm_main.c
+++ srcs/assembler/asm_main.c
@@ -6,7 +6,7 @@
 /*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 13:21:32 by gemerald          #+#    #+#             */
-/*   Updated: 2020/03/07 13:22:10 by gemerald         ###   ########.fr       */
+/*   Updated: 2020/06/26 20:16:19 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,21 @@ void		init_functions(int (*actions[])(char *, t_flag *), t_flag *flag)
 	actions[1] = &asm_s;
 	actions[2] = &dizasm;
 	actions[3] = &err_usage;
-	(*flag).flag_a = 0; 			//есть ли флаг -а
-	(*flag).visible = 0;			//?
-	(*flag).change_name = 0; 		//есть ли флаг смены имени файла
-	flag->file_name = NULL;			//старое имя. Всегда можно использовать
-	flag->new_file_name = NULL;		//новое имя если есть
+	(*flag).flag_a = 0;
+	(*flag).visible = 0;
+	(*flag).change_name = 0;
+	flag->file_name = NULL;
+	flag->new_file_name = NULL;
 }
 
 int			main(int ac, char **av)
 {
-	t_flag	has_flag; 					//структура для работы с флагом
-	int		arg_count; 				//для отслеживания кол-ва аргументов
-	int	 	(*actions[4])(char *, t_flag *);
+	t_flag	has_flag;
+	int		arg_count;
+	int		(*actions[4])(char *, t_flag *);
 
 	arg_count = 1;
-	init_functions(actions, &has_flag); 	// + инициализация значений для структуры флага
+	init_functions(actions, &has_flag);
 	actions[take_name(ac, av, &arg_count, &has_flag)](av[arg_count], &has_flag);
 	return (0);
 }
