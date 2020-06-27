7b0bb196e8954dd76c6dedd10f3e024dedb4153d
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-27 17:17:43 +0300

    generic_list_part2.c norme

--- srcs/assembler/generic_list_part2.c
+++ srcs/assembler/generic_list_part2.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   generic_list_part2.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcaesar  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 23:39:03 by lcaesar           #+#    #+#             */
-/*   Updated: 2020/06/23 23:39:45 by lcaesar          ###   ########.fr       */
+/*   Created: 2020/06/27 17:16:46 by gemerald          #+#    #+#             */
+/*   Updated: 2020/06/27 17:17:19 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
-
 
 t_generic_list	*get_last_element(t_generic_list *list)
 {
@@ -26,7 +25,7 @@ t_generic_list	*get_last_element(t_generic_list *list)
 }
 
 t_generic_list	*concatenate_lists(t_generic_list *base_list,
-t_generic_list *augmenting_list, t_generic_list *last_element)
+		t_generic_list *augmenting_list, t_generic_list *last_element)
 {
 	if (!base_list)
 		return (augmenting_list);
