191e4cd1b498e182fcd37d5442d49ed39e7989a1
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-27 19:32:00 +0300

    generic_list.h

--- includes/generic_list.h
+++ includes/generic_list.h
@@ -6,7 +6,7 @@
 /*   By: lcaesar <lcaesar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 17:33:37 by lcaesar           #+#    #+#             */
-/*   Updated: 2020/02/13 17:51:33 by lcaesar          ###   ########.fr       */
+/*   Updated: 2020/06/27 19:31:42 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ t_generic_list	*new_generic_list(void *stuff);
 t_generic_list	*add_to_list(t_generic_list *list, void *stuff);
 int				count_generic_list_objects(t_generic_list *generic_list);
 int				is_on_the_list(t_generic_list *generic_list, void *stuff);
-void			destroy_generic_list(t_generic_list **generic_list, void (*destructor)(void *));
+void			destroy_generic_list(t_generic_list **generic_list,
+				void (*destructor)(void *));
 
 t_generic_list	*get_last_element(t_generic_list *list);
 t_generic_list	*concatenate_lists(t_generic_list *base_list,
