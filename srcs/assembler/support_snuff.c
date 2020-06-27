ea60d92e2b0e47406fde2a3596e0aa587e856812
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-27 17:44:02 +0300

    support_snuff.c norme

--- srcs/assembler/support_snuff.c
+++ srcs/assembler/support_snuff.c
@@ -3,42 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   support_snuff.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcaesar  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 13:00:49 by lcaesar           #+#    #+#             */
-/*   Updated: 2020/06/23 13:01:42 by lcaesar          ###   ########.fr       */
+/*   Created: 2020/06/27 17:41:54 by gemerald          #+#    #+#             */
+/*   Updated: 2020/06/27 17:43:43 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-char    *get_string_of_chars(unsigned int size, char c)
+char	*get_string_of_chars(unsigned int size, char c)
 {
-	char            *string;
-	unsigned int    n;
+	char			*string;
+	unsigned int	n;
 
 	string = ft_strnew(size);
 	n = 0;
 	while (n < size)
 	{
 		string[n] = c;
-		n ++;
+		n++;
 	}
 	return (string);
 }
 
-char    *join_and_free(char *lhs, char *rhs)
+char	*join_and_free(char *lhs, char *rhs)
 {
 	char *result;
 
 	result = ft_strjoin(lhs, rhs);
 	free(lhs);
 	free(rhs);
-
 	return (result);
 }
 
-char    *pad_with_chars(char *string, unsigned int pad_size, char c, int side)
+char	*pad_with_chars(char *string, unsigned int pad_size, char c, int side)
 {
 	char *result;
 	char *pad_string;
@@ -50,14 +49,13 @@ char    *pad_with_chars(char *string, unsigned int pad_size, char c, int side)
 	else if (side == 1)
 		result = concat(string, pad_string);
 	free(pad_string);
-
 	return (result);
 }
 
-int     is_string_in_array(char *string, const char **string_array)
+int		is_string_in_array(char *string, const char **string_array)
 {
-	int     n;
-	const char    *current_string;
+	int				n;
+	const char		*current_string;
 
 	n = 0;
 	current_string = string_array[0];
