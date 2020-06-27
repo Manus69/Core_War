67c013a2ef278a4a8c88b6919236c68c0d73bd4d
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-27 17:19:52 +0300

    is_it_fun.c norme

--- srcs/assembler/is_it_fun.c
+++ srcs/assembler/is_it_fun.c
@@ -3,36 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   is_it_fun.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcaesar  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 23:39:03 by lcaesar           #+#    #+#             */
-/*   Updated: 2020/06/23 23:39:45 by lcaesar          ###   ########.fr       */
+/*   Created: 2020/06/27 17:18:18 by gemerald          #+#    #+#             */
+/*   Updated: 2020/06/27 17:19:35 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int     is_quotation_mark(char *string)
+int	is_quotation_mark(char *string)
 {
 	return (check_symbol(string, '"'));
 }
 
-int     is_comment_character(char *string)
+int	is_comment_character(char *string)
 {
-	return (check_symbol(string, COMMENT_CHAR) || check_symbol(string, COMMENT_CHAR_2));
+	return (check_symbol(string, COMMENT_CHAR) ||
+			check_symbol(string, COMMENT_CHAR_2));
 }
 
-int     is_new_line(char *string)
+int	is_new_line(char *string)
 {
 	return (check_symbol(string, '\n'));
 }
 
-int     is_argument_separator(char *string)
+int	is_argument_separator(char *string)
 {
 	return (check_symbol(string, SEPARATOR_CHAR));
 }
 
-int     is_operation(char *string)
+int	is_operation(char *string)
 {
-   return (is_string_in_array(string, g_operation_names));
+	return (is_string_in_array(string, g_operation_names));
 }
