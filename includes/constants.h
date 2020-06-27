a412494cf895d430c96477fe2b0e150522d765b4
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-27 19:23:15 +0300

    constants.h norme

--- includes/constants.h
+++ includes/constants.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   constants.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/27 19:21:21 by gemerald          #+#    #+#             */
+/*   Updated: 2020/06/27 19:23:00 by gemerald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 
@@ -13,25 +25,18 @@
 # define NUMBER_SYSTEM_BASE 16
 # define PADDING_SIZE 4
 # define BUFFER_SIZE 1024
-
-//depreciated; 
-// # define ARG_ENCODING_SIZE 1 //is there a constant in the header already?
-// # define REG_ARG_SIZE 1
-// # define DIR_ARG_SIZE 2
-// # define IND_ARG_SIZE 2 //how much is it really?
-//
-
 # define REGISTRY_NAME_SIZE T_REG
 # define REGISTRY_SIZE REG_SIZE
 # define DIRECT_VALUE_SIZE REGISTRY_SIZE
 # define DIRECT_ADDRESS_SIZE IND_SIZE
 # define INDIRECT_VALUE_SIZE IND_SIZE
 
-
-static const char		*g_operation_names[] = {"live", "ld", "st", "add", "sub", "and",
-"or", "xor", "zjmp", "ldi", "sti", "fork", "lld", "lldi", "lfork", "aff", 0};
+static const char		*g_operation_names[] = {"live", "ld", "st", "add",
+	"sub", "and", "or", "xor", "zjmp", "ldi", "sti", "fork", "lld", "lldi",
+	"lfork", "aff", 0};
 static const char		*g_command_names[] = {".name", ".comment", 0};
-static const char		g_separators[] = {'"', '\n', COMMENT_CHAR, COMMENT_CHAR_2, SEPARATOR_CHAR, 0}; //null byte to terminate the array;
+static const char		g_separators[] = {'"', '\n',
+	COMMENT_CHAR, COMMENT_CHAR_2, SEPARATOR_CHAR, 0};
 static const char		g_spaces[] = {' ', '\t', 0};
 static const char		g_comment_chars[] = {COMMENT_CHAR, COMMENT_CHAR_2, 0};
 
