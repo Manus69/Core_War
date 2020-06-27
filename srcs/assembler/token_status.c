fc4e6c2839b32cd699043ec03818cc765ac76371
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-27 18:05:03 +0300

    token_status.c norme

--- srcs/assembler/token_status.c
+++ srcs/assembler/token_status.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_status.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/27 18:04:22 by gemerald          #+#    #+#             */
+/*   Updated: 2020/06/27 18:04:43 by gemerald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "asm.h"
 
 enum e_status_code	get_status_code(t_token *token)
@@ -17,6 +29,6 @@ void				set_status(t_container *container, t_token *token)
 	status_code = get_status_code(token);
 	shifted_number = (1 << status_code);
 	if (container->status & shifted_number)
-		invoke_error("token duplication\n", token, NULL, container); //msg
+		invoke_error("token duplication\n", token, NULL, container);
 	container->status = (container->status | shifted_number);
 }
