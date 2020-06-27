f486b1471906950b54457e95c12d266e5715cc17
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-06-27 17:24:26 +0300

    measure_size.c norme

--- srcs/assembler/measure_size.c
+++ srcs/assembler/measure_size.c
@@ -3,31 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   measure_size.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcaesar  <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 12:53:15 by lcaesar           #+#    #+#             */
-/*   Updated: 2020/06/23 12:57:23 by lcaesar          ###   ########.fr       */
+/*   Created: 2020/06/27 17:22:37 by gemerald          #+#    #+#             */
+/*   Updated: 2020/06/27 17:24:07 by gemerald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static void     measure_arg_size(t_token *token,
-enum e_operation_name operation_type)
+static void	measure_arg_size(t_token *token,
+		enum e_operation_name operation_type)
 {
 	if (token->argument_type == registry)
 		token->size = REGISTRY_NAME_SIZE;
 	else if (token->argument_type == direct)
-		token->size = g_op_tab[operation_type].size_t_dir ? DIRECT_ADDRESS_SIZE : DIRECT_VALUE_SIZE;
+		token->size = g_op_tab[operation_type].size_t_dir ?
+			DIRECT_ADDRESS_SIZE : DIRECT_VALUE_SIZE;
 	else if (token->argument_type == indirect)
 		token->size = INDIRECT_VALUE_SIZE;
 }
 
-void            measure_token_size(t_generic_list *tokens)
+void		measure_token_size(t_generic_list *tokens)
 {
-	enum e_operation_name   operation_type;
-	t_generic_list          *current_token;
-	t_token                 *token;
+	enum e_operation_name	operation_type;
+	t_generic_list			*current_token;
+	t_token					*token;
 
 	current_token = tokens;
 	operation_type = dummy_operation;
