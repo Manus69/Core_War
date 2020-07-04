3d38a7dbea7573388d0f19d0058167c74e945ded
Grass Emerald <gemerald@mi-o5.msk.21-school.ru>
2020-07-04 20:47:21 +0300

    tables.h norme

--- includes/tables.h
+++ includes/tables.h
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tables.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gemerald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/04 20:46:25 by gemerald          #+#    #+#             */
+/*   Updated: 2020/07/04 20:47:04 by gemerald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef TABLES_H
 # define TABLES_H
 
-#include "op.h"
+# include "op.h"
 
 static const char		*const g_operation_names[] = {"live", "ld", "st", "add",
 	"sub", "and", "or", "xor", "zjmp", "ldi", "sti", "fork", "lld", "lldi",
@@ -11,7 +23,8 @@ static const char		g_separators[] = {'"', '\n',
 	COMMENT_CHAR, COMMENT_CHAR_2, SEPARATOR_CHAR, 0};
 static const char		g_spaces[] = {' ', '\t', 0};
 static const char		g_comment_chars[] = {COMMENT_CHAR, COMMENT_CHAR_2, 0};
-static const char		*const g_error_messages[] = {"The name size is wrong;\n",
+static const char		*const g_error_messages[] =\
+{"The name size is wrong;\n",
 	"The comment size is wrong;\n", "The champ size is wrong;\n",
 	"The argument is of the wrong type;\n",
 	"The arg count is wrong;\n",
